340ec849595afc5bf63974e49e6268f6edb3b20a
arlz1013 <jdavid.pforero@gmail.com>
2023-03-08 17:13:35 -0500

    LogreESOGN

--- Scripts/GetDatas.js
+++ Scripts/GetDatas.js
@@ -1,4 +1,13 @@
-import data from '../json/DatasShow.json' assert {type: 'json'}
-
-
-console.log(data['Tema1 '])
+var app = {};
+var Temas = datos=>
+{
+    
+    console.log(datos);
+    app.DatasShow = datos;
+    var html = "";
+    html+="<h2>Datos<h2>";
+    app.DatasShow.map(dts=>{
+        html+="<h1> "+dts.NombreTema+"</h1>";
+    })
+    document.getElementById("Defaults").innerHTML = html;
+}
